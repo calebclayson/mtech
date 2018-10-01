02fcae076b2f34b439da02d140845d8d4aa72324
clayson.caleb@gmail.com <clayson.caleb@gmail.com>
2018-10-01 10:17:44 -0600

    made a reflector function that prints out properties of functions and methods of functions

--- javascript/js/businessCard.js
+++ javascript/js/businessCard.js
@@ -24,3 +24,31 @@ function Phantom(myx, myy, myname) {
         console.log(`${this.name} ate ${food}`);
     }
 }
+
+let Reflector = function(obj) {
+    this.getProperties = function() {
+        let properties = [];
+        for(let prop in obj) {
+            if(typeof obj[prop] != "function") {
+                properties.push(prop);
+            }
+        }
+        return properties;
+    };
+    this.getMethods = function() {
+        let methods = [];
+        for (let method in obj) {
+            if(typeof obj[method] == "function") {
+                methods.push(method);
+            }
+        }
+        return methods;
+    }
+};
+
+let myphantom = new Phantom();
+let mirror = new Reflector(myphantom);
+let myprops = mirror.getProperties();
+let mymethods = mirror.getMethods();
+console.log(myprops);
+console.log(mymethods);
