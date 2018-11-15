98da3989b88f2c63f2fa06fd493ee78d5e55cbea
clayson.caleb@gmail.com <clayson.caleb@gmail.com>
2018-11-15 11:12:08 -0700

    created a search function to find a value in an array and commented out functions to decrease load

--- sort/berkley.js
+++ sort/berkley.js
@@ -1,27 +1,27 @@
 
 
-function boysort(ary) {
-    let sorted = false;
-    let head = [];
+// function boysort(ary) {
+//     let sorted = false;
+//     let head = [];
        
-    if(ary.length > 1){
-        for(h = 0; h > ary.length; h++) {
+//     if(ary.length > 1){
+//         for(h = 0; h > ary.length; h++) {
 
-        }
-        let x = ary.pop();
-        let y = ary.pop();
-            if(x > y){
-                head.push(x);                
-            }
-            if(x < y){
-                head.push(y);
-            }
+//         }
+//         let x = ary.pop();
+//         let y = ary.pop();
+//             if(x > y){
+//                 head.push(x);                
+//             }
+//             if(x < y){
+//                 head.push(y);
+//             }
         
                        
             
-    }
+//     }
 
     
-}
+// }
 
-console.log('boy sort', boysort(ary));
+// console.log('boy sort', boysort(ary));
--- sort/caleb.js
+++ sort/caleb.js
@@ -24,3 +24,32 @@ function bubbleSort(arr) {
 }
 
 console.log('bubbleSort: ', bubbleSort(ary));
+
+let newSortedAry = bubbleSort(ary);
+
+function search(arr, num) {
+    let done = false;
+    let min = 0;
+    let max = arr.length - 1;
+    let mid = Math.round((arr.length-1)/2);
+
+    while(!done) {
+        if(arr[mid] == num) {
+            done = true;
+            console.log('array is '+num+" at index of "+mid);
+            return true;
+        } else if(arr[mid] > num) {
+            max = mid;
+            mid = Math.round(((min + max)/2));
+        } else if(arr[mid] < num) {
+            min = mid;
+            mid = Math.round((min + max)/2);
+        } else {
+            console.log('not in array');
+            done = true;
+            return false;
+        }
+    }
+}
+
+search(newSortedAry, 3);
